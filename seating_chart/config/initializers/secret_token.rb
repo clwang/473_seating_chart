19c9548464a1f9b6065a207323ad1bf10a6d8e3c
Charles Wang <cwang@realpractice.com>
2011-03-19 20:03:17 -0700

    first commit  for the seating application

---
+++ seating_chart/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeatingChart::Application.config.secret_token = '59470eef164ec876606a834ec0e3aa66aadb55db788dc7737219b833cbaa025aaf0fdae9cc1e77fc1fe799f5b6f6881a155b0ec8082ebabea42e707dab76e4f5'
