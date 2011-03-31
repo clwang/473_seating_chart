963a5153b7ade31056dbf9f9e9eaf83b32e1a634
Chad_inc <chadincorporated@gmail.com>
2011-03-30 22:32:49 -0700

    tried my hand at installing Pusher

--- seating_chart/app/controllers/chats_controller.rb
+++ seating_chart/app/controllers/chats_controller.rb
@@ -1,2 +1,12 @@
 class ChatsController < ApplicationController
+  def add_line
+      @update = {:entry => params[:entry], :name => params[:name]}
+
+      Pusher['chat'].trigger('add_line', @update)
+
+      render :update do |page|
+        page[:entry].clear
+        page[:entry].focus
+      end
+    end
 end
---
+++ seating_chart/config/initializers/pusher.rb
@@ -0,0 +1,3 @@
+Pusher.app_id = '4815'
+Pusher.key = 'dfc37692cce3244d3b5a'
+Pusher.secret='e08f6cbb6194db1ba54f'
