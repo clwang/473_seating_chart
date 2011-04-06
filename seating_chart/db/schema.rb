cde88644effd4bf591a79c9ec16d8c088e905de4
Chad_inc <chadincorporated@gmail.com>
2011-04-05 23:18:30 -0700

    removed the random files that I created during testing, removeed extra unneeded gem files.

--- seating_chart/db/schema.rb
+++ seating_chart/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110321001545) do
+ActiveRecord::Schema.define(:version => 20110330232911) do
+
+  create_table "chats", :force => true do |t|
+    t.string   "name"
+    t.string   "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "seats", :force => true do |t|
     t.integer  "seat_number"
